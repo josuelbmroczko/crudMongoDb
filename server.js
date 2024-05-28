51dc5e383fa62dfd677f31d4dd5d14dcb3d8c051
josuelbmroczko <josuelbeserramroczko@gmail.com>
2024-05-28 17:14:13 -0300

    crudmongodb

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-import app from "./src/app.js";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const PORT = 3000;
+const app = express();
 
-app.listen(PORT, () => {
-    console.log("servidor escutando");
+// Estas duas linhas são necessárias para usar __dirname com ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Servir os arquivos estáticos do aplicativo React
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// Rota de API para fornecer dados
+app.get('/api/data', (req, res) => {
+  const data = { message: "Hello from the server!" };
+  res.json(data);
 });
 
-//mongodb+srv://admin:admin123@cluster0.volyzjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// Rota principal - Servir o aplicativo React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor Node.js rodando na porta ${PORT}`);
+});
